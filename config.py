f3258177295b2d77abe73eac35cc996b9241ad6f
Nero <gtnero@proton.me>
2025-11-16 16:35:55 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,10 @@
-API_ID = 123456  # Replace with your API ID
-API_HASH = "your_api_hash"
-BOT_TOKEN = "your_bot_token"
-# Owner Telegram user IDs (for /logs access etc.)
-OWNER_ID = [123456789, 987654321]  # Replace with your actual Telegram user IDs
+import os
 
-# Format: source_chat_id ➝ target_chat_id
-#FORWARD_MAP = {
-#    -1001234567890: -1009876543210,  # A ➝ B
-#    -1001122334455: -1005544332211,  # C ➝ D
-#}
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+OWNER_ID = list(map(int, os.getenv("OWNER_ID", "").split(",")))
 
-# IDs of Stickers, you can add yours too
 STICKER_IDS = [
     "CAADAQADtQIAAqVCUEemuIH_uo6q8wI",
     "CAADAQAD3wADkTtARaGVp5D7hcquAg",
